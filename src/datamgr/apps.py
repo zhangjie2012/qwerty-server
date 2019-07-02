f77daff01ad997b102f63e2fab43ac2b9bcbc938
JerryZhang <me@zhangjiee.com>
2019-07-02 22:36:33 +0800

    remove datamgr, import/export will implement by qwerty-tools

--- src/datamgr/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatamgrConfig(AppConfig):
-    name = 'datamgr'
--- src/datamgr/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('import_jekyll_content', views.import_jekyll_content),
-]
